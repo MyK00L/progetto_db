30de6b271631c7ed2fbfc222703cc5a3f2286cd2
Eugenio Tampieri <eugenio.tampieri@studio.unibo.it>
2022-01-21 20:02:19 +0100

    Actually bring up DB

---
+++ src/db.rs
@@ -0,0 +1,33 @@
+use once_cell::sync::Lazy;
+use r2d2_postgres::{postgres::NoTls, PostgresConnectionManager};
+
+pub static POOL: Lazy<r2d2::Pool<PostgresConnectionManager<NoTls>>> = Lazy::new(|| {
+    let manager = PostgresConnectionManager::new(
+        format!(
+            "host={} user={} password={} dbname={}",
+            std::env::var("DB_HOST").expect("DB_HOST undefined"),
+            std::env::var("DB_USER").expect("DB_USER undefined"),
+            std::env::var("DB_PASSWORD").expect("DB_PASSWORD undefined"),
+            std::env::var("DB_NAME").expect("DB_NAME undefined"),
+        )
+        .parse()
+        .unwrap(),
+        NoTls,
+    );
+    r2d2::Pool::new(manager).unwrap()
+});
+pub fn create_db() -> Result<(), String> {
+    let pool = POOL.clone();
+    let mut client = pool.get().map_err(|x| format!("{:?}", x))?;
+    let queries = include_str!("../queries/create_db.sql")
+        .split(';')
+        .map(|x| x.trim())
+        .filter(|x| x.len() > 0)
+        .map(|x| x.replace('§', ";"));
+    for query in queries {
+        client
+            .execute(&format!("{};", query), &[])
+            .map_err(|x| format!("{:?}", x))?;
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+#[macro_use]
+extern crate rocket;
+mod db;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    dotenv::dotenv().ok();
+    std::thread::spawn(|| {
+        db::create_db().expect("Failed to create DB");
+    });
+    rocket::build().mount("/", routes![index])
+}
