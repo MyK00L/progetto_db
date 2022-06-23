c1b86ed89c636daf7fc11651d1be579108d8a622
Eugenio Tampieri <eugenio@eutampieri.eu>
2022-06-23 22:36:08 +0200

    Added home page

--- src/routes.rs
+++ src/routes.rs
@@ -213,3 +213,9 @@ pub async fn insert_api(stuff: Form<Strict<InsertItem>>, db: crate::DbConn) -> S
     eprintln!("{:?}", ans);
     String::from("halp")
 }
+
+#[get("/")]
+pub async fn home() -> Template {
+    let ctx: std::collections::HashMap<u8, u8> = Default::default();
+    Template::render("home", ctx)
+}
