3b33eeb97de182ef1034dc2480c233642d4be340
Michael Chelli <michaelchelli00@gmail.com>
2022-06-25 10:35:24 +0200

    trygetftw

--- src/utils.rs
+++ src/utils.rs
@@ -1,27 +1,24 @@
 use std::fmt::Display;
 
-pub fn get_sql<I>(row: &postgres::row::Row, idx: I, sql_type: &str) -> String
+pub fn get_sql<I>(row: &postgres::row::Row, idx: I) -> String
 where
     I: postgres::row::RowIndex + Display,
 {
-    match sql_type {
-        "BOOL" => {
-            let x: bool = row.get(idx);
-            match x {
-                true => "V",
-                false => "F",
-            }
-            .to_string()
-        }
-        "INT" => {
-            let x: i32 = row.get(idx);
-            format!("{}", x)
-        }
-        "REAL" => {
-            let x: f32 = row.get(idx);
-            format!("{}", x)
-        }
-        "TEXT" => row.get(idx),
-        _ => panic!(), //Default::default(),
+    if let Ok(x) = row.try_get::<_, String>(idx) {
+        return x;
     }
+    if let Ok(x) = row.try_get::<_, i32>(idx) {
+        return format!("{}", x);
+    }
+    if let Ok(x) = row.try_get::<_, f32>(idx) {
+        return format!("{}", x);
+    }
+    if let Ok(x) = row.try_get::<_, bool>(idx) {
+        return match x {
+            true => "V",
+            false => "F",
+        }
+        .to_string();
+    }
+    panic!();
 }
